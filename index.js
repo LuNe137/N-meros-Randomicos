1085ca7f268120d819bb1d54109037c56472f436
LuNe137 <uizsk8@gmail.com>
2020-01-31 15:28:22 -0300

    upando codigo

---
+++ index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View, Button, AppRegistry } from 'react-native'
+
+
+const gerarNumeroAleatorio = () => {
+	var numeros_randomicos = Math.random();
+	numeros_randomicos = Math.floor(numeros_randomicos * 10);
+	alert(numeros_randomicos);
+}
+
+const App = () => {
+
+	return (
+		<View>
+		<Text>
+		Gerador de números randomicos
+		</Text>
+		<Button title = "Gerar número randomico"
+		onPress = {gerarNumeroAleatorio}
+		/>
+		</View>
+	); 
+};
+
+AppRegistry.registerComponent('teste', () => App);
